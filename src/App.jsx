26a40fdb8c0137e0870af011a5fa284dbec0e600
jchanda1 <joyanto.chanda@gmail.com>
2025-06-24 12:01:29 +0100

    add layout to web page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import "./App.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Layout from "./UI/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <NavBar />
-        <main className="flex-grow pt-3">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer className="mt-auto" />
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
